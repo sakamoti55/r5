90a03b7b72a66d5627482688e5bdff7437c3a850
sakamoti55 <kohseimeimei@gmail.com>
2023-12-03 02:38:46 +0900

    Readme,国の左右変更ボタンの実装(mobile.cssは使用不可)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 
 //A国での通貨をB国での通貨価値に換算する
-async function fetchCategory(data, Acountry, Bcountry) {
-    const Arate = data.conversion_rates[Acountry];
-    const Brate = data.conversion_rates[Bcountry];
+async function fetchCountry(data, keepAcountry, keepBcountry) {
+    const Arate = data.conversion_rates[keepAcountry];
+    const Brate = data.conversion_rates[keepBcountry];
 
     const ratio = parseFloat(Brate) / parseFloat(Arate);
     return ratio;
@@ -15,9 +15,10 @@ export default function App() {
     
     const [exchangedAmount,setExchangedAmount] = useState();
     const [amount, setAmount] = useState("");
+    const [keepAcountry, setKeepAcountry] = useState("USD");
+    const [keepBcountry, setKeepBcountry] = useState("USD");
     const [Acountry, setAcountry] = useState("USD");
     const [Bcountry, setBcountry] = useState("USD");
-
     //開いたとき、変更を加えたときに処理
     useEffect(() => {
         //関数
@@ -34,30 +35,41 @@ export default function App() {
     async function handleClick(event) {
         event.preventDefault();
         
-        const newRatio = await fetchCategory(data, Acountry, Bcountry);    
+        const newRatio = await fetchCountry(data, keepAcountry, keepBcountry);    
         const roundedValue = parseFloat(((amount) * newRatio).toFixed(2));    
         setExchangedAmount(roundedValue);
+        setAcountry(keepAcountry);
+        setBcountry(keepBcountry);
     }
-    
 
+    function changeCountry(event) {
+      event.preventDefault();
+      const keep = keepAcountry;
+      setKeepAcountry(keepBcountry);
+      setKeepBcountry(keep);
+
+
+    }
 
   return (
     <>
       <header>
-        <h1>Exchange money</h1>
+        <h1>Money Exchange</h1>
       </header>
       <div>
         <aside>
-          <form>
+          <form class="form1">
+            {/*label表示*/}
             <div class="div0">
               <label htmlFor="category">Choose a Country to Country</label> 
             </div>
+            {/*国変更*/}
             <div class="div1">
               <select class="a"
                 id="category" 
                 //asyncは必要ない
-                onChange={(event) => setAcountry(event.target.value)
-                }
+                onChange={(event) => setKeepAcountry(event.target.value)}
+                value={keepAcountry}//valueを用いて表示を変更する
               >
                 {data && Object.keys(data.conversion_rates).map((countryCode) => (
                     <option key={countryCode} value={countryCode}>
@@ -70,11 +82,9 @@ export default function App() {
 
               <select class="b"
                 id="category2" 
-                onChange={(event) => setBcountry(event.target.value)
-                }
-
+                onChange={(event) => setKeepBcountry(event.target.value)}
+                value={keepBcountry}//valueを用いて表示を変更する
               >
-
                 {data && Object.keys(data.conversion_rates).map((countryCode) => (
                     <option key={countryCode} value={countryCode}>
                         {countryCode}
@@ -82,46 +92,61 @@ export default function App() {
                 ))}
               </select>
             </div>
+            {/*国切り替えボタン*/}
             <div class="div2">
+              {/*Enterキーでの誤実行を防ぐための処置 */}
+              <form>
+                <button onClick={changeCountry}>⇌</button>
+              </form>
+              
+            </div>
+            {/**/}
+            <div class="div3">
               <label htmlFor="enterAmount">Enter amount:</label>
-              <input 
-                type="text" 
-                id="enterAmount" 
-                placeholder="num" 
-                value={amount}
-                onChange={(event) => setAmount(event.target.value) 
-              }         
-              >
-              </input>           
-              <button onClick={handleClick}>Filter results</button>
+                <input 
+                  type="text" 
+                  id="enterAmount" 
+                  placeholder="num" 
+                  value={amount}
+                  onChange={(event) => setAmount(event.target.value) 
+                }         
+                >
+                </input>
+              <form>         
+                <button onClick={handleClick}>Filter results</button>
+              </form>
             </div>
           </form>
         </aside>
       </div>
-      <div>
+      <div class="maindiv">
         <main>
+          <div class="main1">
             <h2>
-                {Acountry} to {Bcountry}
+                - {Acountry} to {Bcountry}
             </h2>
             {/* &&は左辺評価→右辺評価　右辺が真なら右辺の結果を出力 */} 
             {exchangedAmount !== null && (
-            <p>
-              Exchanged Amount: {exchangedAmount} {Bcountry}
-            </p>
+            <h3>
+              Result: {exchangedAmount} {Bcountry}
+            </h3>
             )}
+          </div>
+          <div class="main2">
             {/*下に主要な国の通貨への両替だけピックアップして書くかも*/}
             <h2>
-              exchange rate
+              - Exchange rate
             </h2>
             {data && (
               <ul>              
-                <li>1$ USD: ${data.conversion_rates.USD}</li>
+                <li>1 USD: ${data.conversion_rates.USD}</li>
 
-                <li>1\ JPY : ${data.conversion_rates.JPY}</li>  
+                <li>1  JPY : ${data.conversion_rates.JPY}</li>  
 
-                <li>1€ EUR: ${data.conversion_rates.EUR}</li>
+                <li>1 EUR: ${data.conversion_rates.EUR}</li>
               </ul> 
             )}
+          </div>
         </main>
       </div>
     </>
